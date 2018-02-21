58bd518fefdc0ab2c09f990e72516cd9a6411ee9
Angad Singh <angads25@gmail.com>
2018-02-22 00:29:25 +0530

    feat: Implemented LabeledSwitchActivity in Demo App

--- Readme.md
+++ Readme.md
@@ -4,6 +4,21 @@ Android Library for Custom Switches.
 ### Developed by
 [Angad Singh](https://www.github.com/angads25) ([@angads25](https://www.twitter.com/angads25))
 
+### Benchmark:
+[![API](https://img.shields.io/badge/API-9%2B-brightgreen.svg?style=flat)](https://android-arsenal.com/api?level=9) <a href="http://www.methodscount.com/?lib=com.github.angads25%3Afilepicker%3A1.1.1"><img src="https://img.shields.io/badge/Methods and size-271 | 43 KB-e91e63.svg"/></a>
+
+### Where to Find:
+[ ![Download](https://api.bintray.com/packages/angads25/maven/filepicker/images/download.svg) ](https://bintray.com/angads25/maven/filepicker/_latestVersion) [![Maven Central](https://maven-badges.herokuapp.com/maven-central/com.github.angads25/filepicker/badge.svg)](https://maven-badges.herokuapp.com/maven-central/com.github.angads25/filepicker) [![Android Arsenal](https://img.shields.io/badge/Android%20Arsenal-FilePicker-blue.svg?style=flat)](http://android-arsenal.com/details/1/3950)
+
+
+### Installation
+
+* Library is also Available in MavenCentral, So just put this in your app dependencies to use it:
+```gradle
+    compile 'com.github.angads25:toggle:1.0.0'
+```
+
 ### Switches Available
 
 * Labeled Switch
+
--- app/src/main/java/com/github/angads25/toggledemo/MainActivity.java
+++ app/src/main/java/com/github/angads25/toggledemo/MainActivity.java
@@ -1,30 +1,43 @@
+/*
+ * Copyright (C) 2018 Angad Singh
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.angads25.toggledemo;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.AppCompatTextView;
-import android.support.v4.content.res.ResourcesCompat;
+import android.view.View;
 
-import com.github.angads25.toggle.LabeledSwitch;
-import com.github.angads25.toggle.interfaces.OnToggledListener;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        LabeledSwitch labeledSwitch = findViewById(R.id.switch_demo);
-        Typeface openSansBold = ResourcesCompat.getFont(MainActivity.this, R.font.open_sans_bold);
-        labeledSwitch.setTypeface(openSansBold);
-        labeledSwitch.setOnToggledListener(new OnToggledListener() {
-            @Override
-            public void onSwitched(LabeledSwitch labeledSwitch, boolean isOn) {
-                ((AppCompatTextView)findViewById(R.id.label_value))
-                        .setText(isOn ? labeledSwitch.getLabelOn() : labeledSwitch.getLabelOff());
+        findViewById(R.id.switch_labeled).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.switch_labeled: {
+                startActivity(new Intent(MainActivity.this, LabeledSwitchActivity.class));
             }
-        });
+        }
     }
 }
